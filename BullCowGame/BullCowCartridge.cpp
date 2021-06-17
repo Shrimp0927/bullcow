5f0e3e37b7f55ec2bff87b1e1ceb4fed2aad759d
Shrimp0927 <nguyenhaidang123123@gmail.com>
2021-06-17 03:01:13 -0700

    added endgame func

--- BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowCartridge.cpp
@@ -5,23 +5,40 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     InitGame();
-    PrintLine(TEXT("Hi! Welcome to the Bull Cows!"));
-    PrintLine(FString::Printf(TEXT("Guess the %i letter word"), HiddenWord.Len()));
-    PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+    if (bGameOver == 1) {
+        GameOver();
+    }
     if (HiddenWord == Input) {
         PrintLine(TEXT("W"));
+        bGameOver = 1;
     } else {
-        PrintLine(TEXT("L"));
+        Lives--;
+        if (Lives > 0) {
+            PrintLine(TEXT("guess again"));
+        } else {
+            PrintLine(TEXT("L"));
+            ClearScreen();
+            PrintLine(TEXT("Press enter to continue"));
+            bGameOver = 1;
+        }
     }
-
 }
 
 void UBullCowCartridge::InitGame() {
     HiddenWord = TEXT("cake");
     Lives = 3;
+    bGameOver = 0;
+    PrintLine(TEXT("Hi! Welcome to the Bull Cows!"));
+    PrintLine(FString::Printf(TEXT("Guess the %i letter word"), HiddenWord.Len()));
+    PrintLine(TEXT("Press enter to continue..."));
+}
+
+void UBullCowCartridge::GameOver() {
+    ClearScreen();
+    InitGame();
 }
